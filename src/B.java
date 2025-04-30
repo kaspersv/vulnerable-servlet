a3a04923b36816989f7233c8b3eb5ad206a4bc78
Kasper Svendsen <kaspersv@github.com>
2025-04-30 10:54:12 +0200

    B: Open file

--- src/B.java
+++ src/B.java
@@ -1,5 +1,14 @@
+import java.io.File;
+import java.io.FileInputStream;
+
 public class B {
     public static void foo(String path) {
-
+        // Vulnerability: Open file at user-controlled path
+        try {
+            File file = new File(path);
+            FileInputStream stream = new FileInputStream(file);
+            stream.close();
+        } catch (Exception e) {
+        }
     }
 }
